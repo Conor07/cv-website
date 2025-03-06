cc104233fb03798be12793592ae6081bd1e8046d
Conor Talbot <conor@Conors-MacBook-Pro-2.local>
2025-03-06 13:28:43 +0000

    Updated responsiveness, updates scroll function

--- src/helpers/scrollToElement.ts
+++ src/helpers/scrollToElement.ts
@@ -2,6 +2,12 @@ export const scrollToElement = (pageName: string) => {
   const elementRef = document.getElementById(pageName);
 
   if (elementRef) {
-    elementRef.scrollIntoView({ behavior: "smooth" });
+    // elementRef.scrollIntoView({ behavior: "smooth" });
+
+    const yOffset = -90; // Header height
+    const y = elementRef.getBoundingClientRect().top + window.scrollY + yOffset;
+
+    console.log(pageName, " y: ", y);
+    window.scrollTo({ top: y, behavior: "smooth" });
   }
 };
