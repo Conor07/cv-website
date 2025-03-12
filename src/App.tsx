ecaecbc6a52a2d93dde1f26f921e14b61b393759
Conor Talbot <conor@Conors-MacBook-Pro-2.local>
2025-03-12 13:41:30 +0000

    Updated home url

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
 
           <Routes>
             <Route
-              path="/"
+              path="/cv-website"
               element={<Home scrollPosition={scrollPosition} />}
             />
           </Routes>
